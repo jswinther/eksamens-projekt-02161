79cc3050492426cfe3ee721af6d29ebe3416c0d0
Jonathan <jonathanswinther@gmail.com>
2019-04-01 14:52:52 +0200

    Update MainFrame.java

--- src/dtu/project/app/MainFrame.java
+++ src/dtu/project/app/MainFrame.java
@@ -1,19 +1,43 @@
 package dtu.project.app;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
-public class MainFrame extends JFrame {
-	
-	
+public class MainFrame {
+
+	private JFrame frame;
 
 	/**
-	 * 
+	 * Launch the application.
 	 */
-	private static final long serialVersionUID = 1L;
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame window = new MainFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the application.
+	 */
+	public MainFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
