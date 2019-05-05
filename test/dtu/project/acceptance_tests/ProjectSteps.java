42230d0d7111ae95da134555ef00ac2a7363b03e
Sebastian <squidmanse@gmail.com>
2019-05-05 20:32:50 +0200

    Udkast til projekt cucumber kode.

--- test/dtu/project/acceptance_tests/ProjectSteps.java
+++ test/dtu/project/acceptance_tests/ProjectSteps.java
@@ -10,6 +10,9 @@ import dtu.project.enums.ProjectType;
 import dtu.project.repo.InMemoryRepository;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ProjectSteps {
 
     ProjectApp PA;
@@ -63,9 +66,48 @@ public class ProjectSteps {
     public void theProjectDoesnTExistInTheListProjects() {
         assertTrue(PA.getProjectList().isEmpty());
     }
-
+    
     @Given("a user want to find a project with the name {string}")
     public void aUserWantToFindAProjectWithTheName(String string) {
         PA.searchProjects(string);
     }
+
+    // Fail test
+
+    @When("user adds a project with name {string}, project type INTERNAL.")
+    public void user_creates_illogical_time_plan(String string) {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    	LocalDateTime startDate = null;
+    	LocalDateTime endDate = null;
+//    	startDate = LocalDateTime.parse(startDate, formatter);
+//    	endDate = LocalDateTime.parse(endDate, formatter);
+//    	startDate = "2021-05-05 13:12"
+//    	endDate = "2019-05-05 13:12";
+    	
+    	
+    }
+
+    @Then("exception is thrown")
+    public void exception_is_thrown(String startDate, String endDate) {
+    	throw new cucumber.api.PendingException();
+    }
+    /*
+     * Test der skal sikre sig mod duplicate
+    @Then("exception is thrown.")
+    public void exceptionIsThrown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+	*/
+//    @When("user adds a project with name {string}, project type INTERNAL.")
+//    public void userAddsAProjectWithNameProjectTypeINTERNAL(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new cucumber.api.PendingException();
+//    }
+//
+//    @Then("the user is now assigned to project as project manager.")
+//    public void theUserIsNowAssignedToProjectAsProjectManager() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new cucumber.api.PendingException();
+//    }
 }
--- use_cases/project_cases.feature
+++ use_cases/project_cases.feature
@@ -8,17 +8,6 @@ Scenario: User adds a project with name, project type and time period.
     When the user adds a project with name "TestProject" and project type INTERNAL.
     And time period "2019-05-05 08:00" to "2019-08-05 23:59".
     Then the project exists in the list of projects.
-    
-Scenario: User adds illegal project time period
-		When the user adds a project with name "TestProject" and project type INTERNAL.
-		And time period "2021-05-05 08:00" to "2019-05-05 08:00".
-		Then exception is thrown.
-		
-Scenario: User creates duplicate project
-		Given a project with name "TestProject", project type INTERNAL.
-		And time period "2019-05-05 08:00" to "2019-08-05 08:00".
-		When user adds a project with name "TestProject", project type INTERNAL.
-		And time period "2019-05-05 08:00" to "2019-08-05 08:00".
 		
 Scenario: User edits a project
     Given a project with name "TestProject", project type INTERNAL.
@@ -35,3 +24,15 @@ Scenario: Removes a project
 Scenario: search for a project
 	Given a user want to find a project with the name "projectname"
 	
+	
+	Scenario: User adds illegal project time period
+		When the user adds a project with name "TestProject" and project type INTERNAL.
+		And time period "2021-05-05 08:00" to "2019-05-05 08:00".
+		Then exception is thrown.
+		
+Scenario: User creates duplicate project
+		Given a project with name "TestProject", project type INTERNAL.
+		And time period "2019-05-05 08:00" to "2019-08-05 08:00".
+		When user adds a project with name "TestProject", project type INTERNAL.
+		And time period "2019-05-05 08:00" to "2019-08-05 08:00".
+		Then exception is thrown
