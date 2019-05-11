b43dc9ea55d9f255bab4cfc42a173a783870ed66
GodOfAllBeings <36410886+GodOfAllBeings@users.noreply.github.com>
2019-05-11 15:11:05 +0200

    Junit i bunden af projecttest (dont work)

--- test/dtu/project/acceptance_tests/ProjectSteps.java
+++ test/dtu/project/acceptance_tests/ProjectSteps.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertTrue;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.PatternSyntaxException;
 
+import org.junit.Test;
+
 public class ProjectSteps {
 
 	ProjectApp PA;
@@ -441,4 +443,98 @@ public class ProjectSteps {
     	Project p = new Project.Builder().setProjectName(string).build();
         assertTrue(PA.isActivityListEmpty(p));
     }
+    
+    @Test
+    public void testAddHours() {
+    	String startDate = "2019-05-05 08:00";
+    	String endDate = "2019-08-05 23:59";
+    	Activity a = new Activity.Builder().build();
+    	String message = "message given";
+    	//PA.addHours(PA.getUser(0), startDate, endDate, activity, message);
+    	if( testAddHours1(PA.getUser(0), startDate, endDate, a,    message) && 
+    		testAddHours2(PA.getUser(0), startDate, endDate, a,    message) &&
+    		testAddHours3(null,          startDate, endDate, a,    message) &&
+    		testAddHours4(PA.getUser(0), null,      endDate, a,    message) &&
+    		testAddHours5(PA.getUser(0), startDate, null,    a,    message) &&
+    		testAddHours6(PA.getUser(0), startDate, endDate, null, message) &&
+    		testAddHours7(PA.getUser(0), startDate, endDate, a,    null)
+    	)
+    		assertTrue(true);
+    	else
+    		assertEquals(1,2);
+    }
+    public boolean testAddHours1(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserListWithAcitivites(start, end) != null) {
+    		return true;
+    	}
+    	else {
+    		return false;	
+    	}
+    }
+    @SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours2(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours3(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours4(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours5(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours6(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean testAddHours7(User u, String start, String end, Activity a, String m) {
+    	PA.addHours(u, start, end, a, m);
+    	if(PA.getUserSchedule(0).contains(a)) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }    
 }
