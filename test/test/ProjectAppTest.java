9914dcb026d66fa8db4021edce9bfb20c3db7bce
jswinther <35611281+jswinther@users.noreply.github.com>
2019-05-11 15:31:47 +0200

    JUnit Test for Project App

---
+++ test/test/ProjectAppTest.java
@@ -0,0 +1,199 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectAppTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testProjectApp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStringListOfE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIntListOfE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStringEListOfE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIntEListOfE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProjectString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProjectInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProjectIntProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProjectStringProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchProjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetActivityProjectStringActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetActivityProjectIntActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActivityProjectString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActivityProjectInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserScheduleString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserScheduleInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActivitiesAssignedTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserListWithAcitivites() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSortByValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProjectList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUserToActivity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddHours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsProjectListEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsActivityListEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
