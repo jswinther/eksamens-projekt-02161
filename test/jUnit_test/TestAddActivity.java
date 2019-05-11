9324c4c5d159116e7ee9477a90a6218113d2dcf5
jswinther <35611281+jswinther@users.noreply.github.com>
2019-05-11 16:38:35 +0200

    Update TestAddActivity.java

--- test/jUnit_test/TestAddActivity.java
+++ test/jUnit_test/TestAddActivity.java
@@ -6,11 +6,29 @@ import org.junit.Test;
 
 import dtu.project.controllers.ProjectApp;
 import dtu.project.entities.Activity;
+import dtu.project.entities.Project;
+import dtu.project.exceptions.DuplicateActivityName;
+import dtu.project.exceptions.DuplicateProjectName;
+import dtu.project.repo.InMemoryRepository;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestAddActivity {
 
 	ProjectApp PA;
 	Activity activity;
+        InMemoryRepository M;
+        
+        public TestAddActivity() {
+            try {
+			M = new InMemoryRepository();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.PA = new ProjectApp(M, M);
+        }
 	
 	@Test
 	public void noInputTest() {
@@ -29,6 +47,15 @@ public class TestAddActivity {
 	@Test
 	public void stringInputTest() {
 		Activity a1 = new Activity.Builder().setActivityName("Activity1").build();
+                Project p = new Project.Builder().setProjectName("project").build();
+            try {
+                PA.addProject(p);
+                PA.addActivity(p, a1);
+            } catch (DuplicateProjectName | DuplicateActivityName ex) {
+                Logger.getLogger(TestAddActivity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            assertTrue(PA.getActivity(p, 0).equals(a1));
+                
 	}
 	
 	@Test
